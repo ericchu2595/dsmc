b35d1702eedbd2c0c5672758f4ca5e0b779172d4
peekachuchu <65094289+peekachuchu@users.noreply.github.com>
2021-01-04 13:09:45 -0500

    Github Assignment #1: Prints Vegetables

---
+++ vegetable_test.py
@@ -0,0 +1,12 @@
+# This file prints 10 vegetables.
+
+print("Potato")
+print("Spinach")
+print("Cabbage")
+print("Carrot")
+print("Cauliflower")
+print("Tomato")
+print("Lettuce")
+print("Pumpkin")
+print("Radish")
+print("Cucumber")
