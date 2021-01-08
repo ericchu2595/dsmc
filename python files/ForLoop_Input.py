911672974f40d052ae45f32f10eb6f19166b8758
peekachuchu <65094289+peekachuchu@users.noreply.github.com>
2021-01-08 11:39:56 -0500

    Takes user input and determines if # is odd/even.

---
+++ python files/ForLoop_Input.py
@@ -0,0 +1,7 @@
+# take user input and if the number is either odd or even
+
+num = int(input('Please enter a number'))
+if num % 2 == 0:
+  print('This number is even!')
+else:
+  print('This number is odd!')
