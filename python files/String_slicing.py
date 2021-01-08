a48eba97fbddbec73cabf8111d1192ed4d63a952
peekachuchu <65094289+peekachuchu@users.noreply.github.com>
2021-01-08 11:26:19 -0500

    Testing

---
+++ python files/String_slicing.py
@@ -0,0 +1,11 @@
+family = ['family1', 'family2', 'family3', 'family4', 'family5', 'family6', 'family7', 'family8', 'family9', 'family10', 'family11', 'family12','family13','family14','family15','family16','family17','family18','family19','family20']
+print(family[0:3])
+print(family[:3])
+print(family[3:])
+print(family[1:3])
+print(family[2:3])
+print(family[0:15])
+print(family[5:9])
+print(family[2:12])
+print(family[5:7])
+print(family[8:])
