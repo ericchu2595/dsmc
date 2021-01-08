ce16fd91788cd4b88223851c20816096f2adb08d
peekachuchu <65094289+peekachuchu@users.noreply.github.com>
2021-01-08 11:30:28 -0500

    This prints out a list.

---
+++ python files/List.py
@@ -0,0 +1,3 @@
+family = ['family1', 'family2', 'family3', 'family4', 'family5', 'family6', 'family7', 'family8', 'family9', 'family10', 'family11', 'family12','family13','family14','family15','family16','family17','family18','family19','family20']
+for name in family:
+  print('Hello' , name + '!', 'I hope you are doing well today.')
